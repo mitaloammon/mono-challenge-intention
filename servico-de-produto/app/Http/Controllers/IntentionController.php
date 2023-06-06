2c464de6f1020bc30a62fb47800c510feb9448a9
Mitalo Ammon <mitaloammon@hotmai.com>
2023-06-06 03:26:31 -0300

    add file in /servico-de-produto

--- servico-de-produto/app/Http/Controllers/IntentionController.php
+++ servico-de-produto/app/Http/Controllers/IntentionController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreintentionRequest;
-use App\Models\intention;
+
 
 class IntentionController extends Controller
 {
 
     public function store(StoreintentionRequest $request)
     {
-        $intention = new intention();
 
         $intention = $request->all();
 
